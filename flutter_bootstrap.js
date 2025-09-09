88db44f40615f6f57a7c4d0ae5c1326e6a7d7acc
Võ Minh Trí <v.trivm7@vinhomes.vn>
2025-09-09 20:34:52 +0700

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1150111670"
+    serviceWorkerVersion: "3534174892"
   }
 });
